2a7b8dca990e67d0880f84035ecd4189e7e74885
nadaalsuhaibani <60797270+nadaalsuhaibani@users.noreply.github.com>
2021-03-12 07:01:15 +0300

    Add files via upload
    config modified

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,16 @@
-module.exports = {
-  apiKey: "AIzaSyCzNvEgBRBul9L6GSX2AUXYortszX9qpSs",
-  authDomain: "social-app-8e4ce.firebaseapp.com",
-  databaseURL: "https://social-app-8e4ce.firebaseio.com",
-  projectId: "social-app-8e4ce",
-  storageBucket: "social-app-8e4ce.appspot.com",
-  messagingSenderId: "781078628460",
-  appId: "1:781078628460:web:11093e9b378b85f120b049",
-  measurementId: "G-TT62QKWG86",
-};
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYKYp-idjfgmhoAWsPKSRGPYOrsh2sFew",
+  authDomain: "neighbors-99819.firebaseapp.com",
+  databaseURL: "https://neighbors-99819-default-rtdb.firebaseio.com",
+  projectId: "neighbors-99819",
+  storageBucket: "neighbors-99819.appspot.com",
+  messagingSenderId: "169591259336",
+  appId: "1:169591259336:web:7fb4618a3bdcfdaf4ba5fd",
+  measurementId: "G-F6L4TQL5D1",
+}
+
+const Firebase = firebase.initializeApp(firebaseConfig)
+
+export default Firebase
